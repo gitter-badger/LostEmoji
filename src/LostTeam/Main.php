656a9a68520c7c326d03601425cbd6454661a4b7
Daniel <daniel.barinak@gmail.com>
2016-05-04 19:12:01 +0200

    Added heart emoji

--- src/LostTeam/Main.php
+++ src/LostTeam/Main.php
@@ -1,3 +1,28 @@
 <?php
 
 namespace LostTeam;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;	
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
+
+class Main extends PluginBase implements Listener{
+
+public function onEnable(){
+
+  $this->getLogger()->notice("Enabled!");
+  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+  public function onChat(PlayerChatEvent $event){
+
+    $player = $event->getPlayer();
+    $heartemoji = "♥";
+    $msg = $event->getMessage();
+    if($msg === "<3" or "i love you"){
+      $event->setMessage($heartemoji);
+    }
+
+}
+    
